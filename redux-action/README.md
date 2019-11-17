e9d388c44501ff7da95b1e8bdf8e308521a885e4
koreanstudent <hn12344@naver.com>
2019-11-17 21:53:40 +0900

    Ducks & redux-actions

---
+++ redux-action/README.md
@@ -0,0 +1,18 @@
+## 작업 환경설정
+
+create-react-app redux-counter
+
+yarn add redux react-redux
+
+actions폴더 - 액션 타입과 액션 생성자 파일을 저장합니다.
+
+components폴더 - 컴포넌트의 뷰가 어떻게 생길지만 담당하는 프리젠테이셔널 컴포넌트를 저장합니다.
+
+containers폴더 - 스토어에 있는 상태를 props로 받아 오는 컨테이너 컴포넌트들을 저장합니다.
+
+reducers폴더 - 스토어의 기본 상태 값과 상태의 업데이트를 담당하는 리듀서 파일들을 저장합니다.
+
+lib폴더 - 일부 컴포넌트에서 함께 사용되는 파일을 저장합니다.
+
+
+
---
+++ redux-action/src/components/Counter.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Counter.css';
+
+// onContextMenu - 우클릭을 하여 메뉴가 열리는 이벤트를 의미, e.preventDefault()를 통해 메뉴가 열리지 않게 된다.
+
+const Counter = ({number, color, index, onIncrement, onDecrement, onSetColor}) => {
+    return (
+        <div 
+            className="Counter" 
+            onClick={() => onIncrement(index)} 
+            onContextMenu={
+                (e) => { 
+                    e.preventDefault(); 
+                    onDecrement(index);
+                }
+            } 
+            onDoubleClick={() => onSetColor(index)}
+            style={{backgroundColor: color}}>
+                {number}
+        </div>
+    );
+};
+
+Counter.propTypes = {
+    number: PropTypes.number,
+    color: PropTypes.string,
+    onIncrement: PropTypes.func,
+    onDecrement: PropTypes.func,
+    onSetColor: PropTypes.func
+};
+
+Counter.defaultProps = {
+    number: 0,
+    color: 'black',
+    onIncrement: () => console.warn('onIncrement not defined'),
+    onDecrement: () => console.warn('onDecrement not defined'),
+    onSetColor: () => console.warn('onSetColor not defined')
+};
+
+export default Counter;
+
