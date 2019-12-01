62f5a3f300c96d7a7a9bd5fd19b5fecfaa77380e
koreanstudent <hn12344@naver.com>
2019-12-01 22:26:05 +0900

    immer

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
 
 const App = () =>{
   const nextId = useRef(1);
@@ -12,11 +13,12 @@ const App = () =>{
   const onChange = useCallback(
     e => {
       const { name, value} = e.target;
-      setForm({
-        ...form,
-        [name]: [value]
-      })
-    }, [form]
+      setForm(
+        produce(draft => {
+          draft[name] =value;
+        })
+      )
+    }, []
   )
 
   // form 등록을 위한 함수
@@ -30,10 +32,11 @@ const App = () =>{
       }
 
       // array에 새 항목 등록
-      setData({
-        ...data,
-        array: data.array.concat(info)
-      })
+      setData(
+        produce(draft => {
+          draft.array.push(info);
+        })
+      )
 
       // form 초기화
       setForm({
@@ -48,10 +51,11 @@ const App = () =>{
   // 항목을 삭제하는 함수
   const onRemove =useCallback(
     id => {
-      setData({
-      ...data,
-      array: data.array.filter(info => info.id !== id)
-      })
+      setData(
+        produce(draft => {
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+        })
+      )
     },
     [data]
   )
