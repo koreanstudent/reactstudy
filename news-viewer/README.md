92664f25b7e4d03e9faf75c7496ec37ab8a24c7a
koreanstudent <hn12344@naver.com>
2019-12-04 21:08:34 +0900

    async, await

--- news-viewer/README.md
+++ news-viewer/README.md
@@ -20,8 +20,36 @@ yarn add react-router-dom 리액트 라우터 적용
 
 NewsPage.js - 라우터를 적용할 페이지
 
+프로젝트의 다양한 곳에서 사용될 수 있는 유틸 함수들은 보통 lib 디렉터리를 만든 후 그 안에 작성
+
 ## axios
 
 가장 많이 사용되고 있는 자바스크립트 HTTP 클라이언트입니다.
 
 HTTP 요청을 Promise기반으로 처리한다.
+
+## async & await
+
+자바스크립트의 비동기 처리 패턴 중 가장 최근에 나온 문법
+
+async function 함수명() {
+    await 비동기_처리_메서드_명 ();
+}
+
+함수의 내부 로직 중 HTTP 통신을 하는 비동기 처리 코드 앞에 await를 붙인다.
+
+주의할점 - 비동기 처리 메서드가 꼭 프로미스 객체를 반환해야한다.
+
+await의 대상이 되는 비동기 처리 코드는 Axios 등 프로미스를 반환하는 API 호출 함수이다.
+
+function fetchItem(){
+    return new Promise(function(resolve, reject){
+        var item = [1,2,3];
+        resolve(items)
+    })
+}
+
+async function logItems() {
+    var resultItems = await fetchItems();
+    
+}
--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -31,27 +32,34 @@ const NewsListBlock = styled.div`
 // };
 
 const NewsList = ({category}) => {
-    const [ articles, setArticles] = useState(null);
-    const [ loading, setLoading] = useState(false);
 
+    const [loading, response, error] = usePromise(() => {
+        const query =category ==='all' ? '' : `&category=${category}`;
+
+        return axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=79f9ed56f7244ed693ca766c507f7c44`,);
+    },[category]);
+
+
+    // const [ articles, setArticles] = useState(null);
+    // const [ loading, setLoading] = useState(false);
 
     // 두 번째 배열이 비어있으면 시작할때만 렌더링 된다.
-    useEffect(() => {
-        // async를 사용하는 함수 따로 선언
-        const fetchData =async () => {
-            setLoading(true);
-            try{
-                // category 값이 무엇인지에 따라 요청할 주소가 동적으로 바뀐다.
-                const query =category ==='all' ? '' : `&category=${category}`;
-                const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=79f9ed56f7244ed693ca766c507f7c44`,);
-                setArticles(response.data.articles);
-            }catch (e){
-                console.log(e);
-            }
-            setLoading(false);
-        } 
-        fetchData();
-    }, [category]);
+    // useEffect(() => {
+    //     // async를 사용하는 함수 따로 선언
+    //     const fetchData =async () => {
+    //         setLoading(true);
+    //         try{
+    //             // category 값이 무엇인지에 따라 요청할 주소가 동적으로 바뀐다.
+    //             const query =category ==='all' ? '' : `&category=${category}`;
+    //             const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=79f9ed56f7244ed693ca766c507f7c44`,);
+    //             setArticles(response.data.articles);
+    //         }catch (e){
+    //             console.log(e);
+    //         }
+    //         setLoading(false);
+    //     } 
+    //     fetchData();
+    // }, [category]);
     
     // 대기 중일 때
     if(loading) {
@@ -59,9 +67,16 @@ const NewsList = ({category}) => {
     }
     // 아직 articles 값이 설정되지 않았을 때
     // articles를 조회하여 해당 값이 현재 null이 아닌지 검사해야한다. 이 작업을 하지 않으면, 아직 데이터가 없을 때 null에는 map 함수가 없기 때문에 렌더링 과정에서 오류 발생
-    if(!articles){
+    if(!response){
         return null;
     }
+
+    if (error) {
+        return <NewsListBlock>에러 발생!</NewsListBlock>
+    }
+
+    const { articles } =response.data;
+    
 
     // articles 값이 유효할 때
     return(
