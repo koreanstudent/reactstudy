4107fd23fcbbbd400bd6b306125891abe0c9ffe2
koreanstudent <hn12344@naver.com>
2019-12-05 21:59:17 +0900

    context api

--- context-tutorial/README.md
+++ context-tutorial/README.md
@@ -5,3 +5,6 @@
 
 ## Context API
 
+Context는 createContext라는 함수를 사용해서 만들며, 이 함수를 호출하면 Provider와 Consumer라는 컴포넌트들이 반환됩니다.
+Provider는 Context 에서 사용 할 값을 설정할 때 사용되고, Consumer 는 나중에 우리가 설정한 값을 불러와야 할 때 사용됩니다.
+
--- context-tutorial/src/contexts/color.js
+++ context-tutorial/src/contexts/color.js
@@ -1,7 +1,32 @@
-import { createContext } from 'react';
+import React,{ createContext, useState } from 'react';
 
 // 새 Context를 만들 때는 createContext 함수를 사용합니다.
-const ColorContext = createContext({ color: 'black'});
+const ColorContext = createContext({ 
+    state: {color: 'black', subcolor: 'red'},
+    actions : {
+        setColor: () => {},
+        setSubcolor: () => {}
+    }
+});
+
+const ColorProvider = ({ children}) => {
+    const [color, setColor] =useState('black');
+    const [subcolor, setSubcolor] = useState('red');
+
+    const value= {
+        state: { color, subcolor },
+        actions : { setColor, setSubcolor}
+    }
+    return (
+        <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+    )
+}
+
+// const ColorConsumer = ColorContext.Consumer와 같은 의미
+const { Consumer: ColorConsumer} = ColorContext;
+
+// ColorProvider와 ColorConsumer 내보내기
+export { ColorProvider, ColorConsumer};
 
 export default ColorContext;
 
