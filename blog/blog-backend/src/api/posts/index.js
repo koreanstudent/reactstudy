7c332c93d9586353826684bbcaae72eed4a7b645
koreanstudent <hn12344@naver.com>
2019-12-08 00:54:27 +0900

    koa

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,22 +1,13 @@
 const Router = require('koa-router');
+const postsCtrl = require('./posts.ctrl');
+
 const posts = new Router();
 
-const printInfo = ctx => {
-
-    ctx.body = {
-        method: ctx.method,
-        path: ctx.path,
-        params: ctx.params
-    }
-
-}
-
-posts.get('/', printInfo.list);
-posts.post('/', printInfo.write);
-posts.get('/:id', printInfo.read);
-posts.delete('/:id', printInfo.remove);
-posts.put('/:id', printInfo.replace);
-posts.patch('/:id', printInfo.update);
-
+posts.get('/', postsCtrl.list);
+posts.post('/', postsCtrl.write);
+posts.get('/:id', postsCtrl.read);
+posts.delete('/:id', postsCtrl.remove);
+posts.put('/:id', postsCtrl.replace);
+posts.patch('/:id', postsCtrl.update);
 
 module.exports = posts;
