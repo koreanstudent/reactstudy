1e19fec94eeaa78e6a7ec5bd19850ca7d97a636b
koreanstudent <hn12344@naver.com>
2019-12-23 22:53:43 +0900

    index.js, App.js 설정

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import PostListPage from './page/PostListPage';
+import LoginPage from './page/LoginPage';
+import RegisterPage from './page/RegisterPage';
+import WritePage from './page/WritePage';
+import PostPage from './page/PostPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route component={PostListPage} path={['/@:username', '/']} exact /> 
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/@:username/:postId" />
+    </>
   );
 }
 
