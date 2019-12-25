46495862e118a66b60e62d28335ff377e8ee13ff
koreanstudent <hn12344@naver.com>
2019-12-25 22:21:18 +0900

    로그인

--- blog/blog-frontend/src/components/common/Button.js
+++ blog/blog-frontend/src/components/common/Button.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import palette from '../../lib/styles/palette';
-
 
 const StyledButton = styled.button`
   border: none;
@@ -16,9 +15,24 @@ const StyledButton = styled.button`
   &:hover {
     background: ${palette.gray[6]};
   }
+  ${props =>
+    props.fullWidth &&
+    css`
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
+  ${props =>
+    props.cyan &&
+    css`
+      background: ${palette.cyan[5]};
+      &:hover {
+        background: ${palette.cyan[4]};
+      }
+    `}
 `;
 
-// {...props}를 styledButton에 설정해 주었는데 이는 Button이 받아 오는 props를 모두 styledButton에 전달한다는 의미
 const Button = props => <StyledButton {...props} />;
 
 export default Button;
