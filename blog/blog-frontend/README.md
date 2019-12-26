43a13679b75f4005b0035ee66761e1b0bd5f474c
koreanstudent <hn12344@naver.com>
2019-12-26 22:19:54 +0900

    리덕스 적용

--- blog/blog-frontend/README.md
+++ blog/blog-frontend/README.md
@@ -5,3 +5,9 @@ yarn add react-router-dom
 yarn add styled-components - 스타일링
 
 yarn add redux react-redux redux-actions immer redux-devtools-extension
+
+page : 라우터에서 사용할 각 페이지 컴포넌트들이 들어 있다.
+
+components : 리덕스 상태에 연결되지 않은 프리젠테이셔널 컴포넌트들이 들어 있다. 각 컴포넌트의 스타일도 이 디렉터리에 넣는다.
+
+containers : 리덕스 상태와 연결된 컨테이너 컴포넌트들이 들어 있다.
--- blog/blog-frontend/src/modules/auth.js
+++ blog/blog-frontend/src/modules/auth.js
@@ -1,18 +1,47 @@
 //리덕스 적용
-
+// 리덕스로 회원가입과 로그인 폼의 상태를 관리하는 방법
 import { createAction, handleActions} from 'redux-actions';
+import produce from 'immer';
 
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+const CHANGE_FIELD ='auth/CHANGE_FIELD';
+const INITIALIZE_FORM ='auth/INITIALIZE_FORM';
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const changeField = createAction(
+    CHANGE_FIELD,
+    ({ form, key, value}) => ({
+        form, //register, login
+        key, // username, password, passwordConfirm
+        value // 실제 바꾸려는 값
+    })
+);
 
-const initialState = {};
+export const initializeForm = createAction(INITIALIZE_FORM, form => form); // register /login
 
+const initialState = {
+    register : {
+        username: '',
+        password: '',
+        passwordConfirm: '',
+    },
+    login : {
+        username: '',
+        password: '',
+    }
+};
+
+// 객체의 구조가 복잡해지거나 객체로 이루어진 배열을 다룰 경우, immer를 사용하면 훨씬 편리하게 상태를 관리 할 수 있다.
 const auth = handleActions(
     {
-        [SAMPLE_ACTION] : (state, action) => state,
+        [CHANGE_FIELD] : (state, { payload: { form, key, value}}) => 
+            produce(state, draft => {
+                draft[form][key] = value; // 예 : state.register.username을 바꾼다.
+            }),
+        [INITIALIZE_FORM] : (state, { payload: form}) => ({
+            ...state,
+            [form]: initialState[form]
+        })
     },
-    initialState
+    initialState,
 );
 
 export default auth;
