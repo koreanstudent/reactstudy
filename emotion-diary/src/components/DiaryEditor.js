a8137cfe8645182ab404044628d65cd1f450c3ff
koreanstudent <hn12344@naver.com>
2024-01-08 00:39:00 +0900

    프로젝트 최적화

--- emotion-diary/src/components/DiaryEditor.js
+++ emotion-diary/src/components/DiaryEditor.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
 import MyHeader from "./MyHeader";
