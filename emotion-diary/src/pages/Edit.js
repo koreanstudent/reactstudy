b886e0b0727456ef036e8cb285cf09fa5cf04aae
koreanstudent <hn12344@naver.com>
2024-01-07 13:55:54 +0900

    react 배열 사용하기

---
+++ emotion-diary/src/pages/Edit.js
@@ -0,0 +1,32 @@
+import { Navigate, useSearchParams } from "react-router-dom";
+const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "winterload" })}>
+        QS바꾸기
+      </button>
+      <button
+        onClick={() => {
+          Navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          Navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
+    </div>
+  );
+};
+
+export default Edit;
---
+++ emotion-diary/src/pages/Home.js
@@ -0,0 +1,67 @@
+import { useContext, useEffect, useState } from "react";
+import { DiaryStateContext } from "../App";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import DiaryList from "../components/DiaryList";
+
+const Home = () => {
+  const diaryList = useContext(DiaryStateContext);
+
+  const [data, setData] = useState([]);
+  const [curDate, setCurDate] = useState(new Date());
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월 `;
+
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정 일기장`;
+  }, []);
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime();
+
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0,
+        23,
+        59,
+        59
+      ).getTime();
+      // js에서 시간을 사용할 때 시 분 초 까지 설정해주어야 한다.//
+
+      setData(
+        diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryList, curDate]);
+
+  useEffect(() => {}, [data]);
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+  return (
+    <div>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
+      <DiaryList diaryList={data} />
+    </div>
+  );
+};
+
+export default Home;
